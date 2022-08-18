1b09e0ed361b07d95cbae9b0b36834e68cc5f4cf
Maksym.info <nagornymaxim@gmail.com>
2022-08-18 13:03:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.querySelector('div.carousel a.left').addEventListener('click', () => {
+    const ul = document.querySelector('ul.items')
+    ul.prepend(ul.querySelector('li:last-of-type'))
+
+    const ulTitles = document.querySelector('ul.titles')
+    ulTitles.prepend(ulTitles.querySelector('li:last-of-type'))
+})
+
+document.querySelector('div.carousel a.right').addEventListener('click', () => {
+    const ul = document.querySelector('ul.items')
+    ul.append(ul.querySelector('li:first-of-type'))
+
+    const ulTitles = document.querySelector('ul.titles')
+    ulTitles.append(ulTitles.querySelector('li:first-of-type'))
+})
