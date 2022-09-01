affc22e877a9f26a1bf5f942c010974072ca1900
Maksym.info <nagornymaxim@gmail.com>
2022-09-01 14:03:23 +0300

    added adaptive to freedom page, to all section except header, footer and carousel
    tried to make carousel

--- js/script-freedom.js
+++ js/script-freedom.js
@@ -1,21 +1,49 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+//
+//     $('.owl-carousel').owlCarousel({
+//         loop: true,
+//         margin: 0,
+//         nav: true,
+//         navText: ['', ' '],
+//         center: true,
+//         responsive: {
+//             0: {
+//                 items: 2.5
+//             },
+//
+//             1000: {
+//                 items: 2.5
+//             }
+//         }
+//     });
+//
+//
+// });
 
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        navText: ['', ' '],
-        center: true,
-        responsive: {
-            0: {
-                items: 3
-            },
-
-            1000: {
-                items: 3
-            }
-        }
-    });
 
 
-});
+
+
+
+const ulCarousel = document.querySelector('.ul-carousel');
+console.log(ulCarousel.lastElementChild);
+
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+
+function slideLeft () {
+    ulCarousel.prepend(ulCarousel.lastElementChild);
+}
+
+function slideRight () {
+    ulCarousel.append(ulCarousel.firstElementChild);
+}
+
+
+arrowLeft.addEventListener('click', slideLeft);
+
+arrowRight.addEventListener('click', slideRight);
+
+
+
+
